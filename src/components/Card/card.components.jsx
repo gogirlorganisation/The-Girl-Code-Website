4fe0aa6d949872db82c15e564d8caa55c0748e1b
Faraz Iqbal <faraziqbal@hotmail.co.uk>
2021-01-19 00:20:05 +0530

    Card Component

--- src/components/Card/card.components.jsx
+++ src/components/Card/card.components.jsx
@@ -1,34 +1,36 @@
 import React from "react";
+import {
+  Title,
+  Subtitle,
+  IconText,
+  IconImg,
+  Description,
+  Img,
+  Button,
+} from "./card.styles";
 
-import { Card as CardComp, Button, Container } from "react-bootstrap";
-const Card = ({
-  image,
-  title,
-  subtitle,
-  iconUrls,
-  iconText,
-  description,
-  isButton,
-}) => {
+import { Card as CardComp, Container } from "react-bootstrap";
+const Card = ({ image, title, subtitle, icons, description, isButton }) => {
+  const Icons = icons.map((icon) => {
+    return (
+      <div style={{ margin: "1rem 0" }}>
+        <IconImg src={icon[0]} alt={icon[1]} /> <IconText>{icon[1]}</IconText>
+      </div>
+    );
+  });
+
   return (
     <Container>
-      <CardComp style={{ width: "24rem", borderRadius: "10px" }}>
-        <CardComp.Img
-          variant="top"
-          src={image}
-          style={{
-            maxWidth: "20rem",
-            padding: "2rem 1rem 0 1rem",
-            margin: "0 auto",
-          }}
-        />
-        <CardComp.Body>
-          <CardComp.Title>{title}</CardComp.Title>
-          <CardComp.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardComp.Text>
-          <Button variant="primary">Go somewhere</Button>
+      <CardComp
+        style={{ width: "20rem", borderRadius: "15px", marginBottom: "5rem" }}
+      >
+        <Img src={image} />
+        <CardComp.Body style={{ marginLeft: "0.5rem" }}>
+          <Title>{title}</Title>
+          <Subtitle>{subtitle}</Subtitle>
+          {Icons}
+          {description ? <Description> {description}</Description> : ""}
+          {isButton ? <Button>View More</Button> : ""}
         </CardComp.Body>
       </CardComp>
     </Container>
