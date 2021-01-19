bbafafbd369d6ab5569ac5930895a5a31da2e227
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2021-01-19 08:00:29 +0530

    Changes

--- src/pages/HomePage/home.page.jsx
+++ src/pages/HomePage/home.page.jsx
@@ -1,8 +1,32 @@
-
 import React from "react";
 
-const Home = () => {
-  return  return <ImageCorousel imgUrls = {['workshop.png', 'workshop2.jpg','workshop3.jpg', 'workshop4.jpg','workshop5.jpg', 'workshop6.jpg','workshop7.jpg','workshop.png', 'workshop2.jpg','workshop3.jpg', 'workshop4.jpg','workshop5.jpg', 'workshop6.jpg','workshop7.jpg']}
-};
+import NavBar from "../../components/NavBar/NavBar.component";
+import GlobalStyle from "../globalStyles";
+import YoutubeVideo from "../../components/YoutubeVideo/YoutubeVideo.component";
+import SocialMediaDiv from "../../components/SocialMediaDiv/SocialMediaDiv.component";
+import LeadCard from "../../components/LeadCard/LeadCard.component";
+import MemberCard from "../../components/MemberCard/MemberCard.component";
+import Team from "../../components/Team/Team.component";
+import Header from "../../components/Header/Header.component";
 
-export default Home;
+  const Home = () => {
+    return (
+      <div>
+        <GlobalStyle />
+        <Header/>
+
+        <NavBar bgOut="transparent" bgIn="#F05680" textOut="black" textIn="white"/>
+        <YoutubeVideo videoOne='https://www.youtube.com/watch?v=ysz5S6PUM-U' 
+                      videoTwo='https://www.youtube.com/watch?v=ysz5S6PUM-U' 
+                      textOne="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
+                      textTwo="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
+      <SocialMediaDiv/>
+      <LeadCard imgUrl="https://source.unsplash.com/Dm-qxdynoEc/800x799" name="Mushroom Boss" designation="Head Of Fungi Development"/>
+      <MemberCard imgUrl="https://source.unsplash.com/Dm-qxdynoEc/800x799" name="Mushroom"/>
+      <Team/>
+      </div>
+    );
+  };
+  export default Home;
+
+
