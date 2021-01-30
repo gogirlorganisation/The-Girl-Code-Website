441ced61b3785749dd56868225f05d5fd271102f
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2021-01-30 13:56:50 +0530

    Whee

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import {
 import Home from "./pages/HomePage/home.page";
 import TeamPage from "./pages/TeamPage/team.page";
 import AllComponents from "./pages/allcomponents";
+import IndividualChapter from "./pages/IndividualChapter/individualchapter.page";
+
 import Form from "./components/form";
 class App extends Component {
   render() {
@@ -19,6 +21,7 @@ class App extends Component {
         <Route exact path="/all" component={AllComponents} />
         <Route exact path="/form" component={Form} />
         <Route exact path="/team" component={TeamPage} />
+        <Route exact path="/individualchapter" component={IndividualChapter} />
       </Router>
     );
   }
